a4a7388712bb32a359352ebd5641ce52ec68ee66
prajwal sadaphal <prajwalsadaphal@gmail.com>
2024-11-29 15:50:58 +0530

    info

--- App.tsx
+++ App.tsx
@@ -5,16 +5,15 @@
  * @format
  */
 
-import React, { useEffect } from 'react';
-import type { PropsWithChildren } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+  ActivityIndicator,
   Alert,
+  FlatList,
   NativeModules,
   PermissionsAndroid,
   Platform,
-  Pressable,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -27,61 +26,55 @@ import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
 import HotSpot from '@react-native-tethering/hotspot';
+import { NetworkInfo } from "react-native-network-info";
 
 
 
 function App(): React.JSX.Element {
+
+  const [connectedDeviceList, setConnectedDeviceList] = useState<Array<string>>([])
+  const [loading, setLoading] = useState(false)
+  const [deviceIpAddress, setDeviceIpAddress] = useState("")
+
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const getPermissions = async () => {
-    try {
-      let writeSettingGranted = await HotSpot.isWriteSettingsGranted()
-      if (writeSettingGranted) {
-        return
-      }
-      await HotSpot.openWriteSettings()
-      
-
-    } catch (error) {
-      console.log("error", JSON.stringify(error))
-    }
-
-  }
 
 
   const requestPermissions = async () => {
     if (Platform.OS === 'android') {
-        try {
-            const granted = await PermissionsAndroid.requestMultiple([
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                PermissionsAndroid.PERMISSIONS.NEARBY_WIFI_DEVICES, 
-            ]);
+      try {
+        const granted = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          PermissionsAndroid.PERMISSIONS.NEARBY_WIFI_DEVICES,
+        ]);
 
-            if (
-                granted[PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION] === PermissionsAndroid.RESULTS.GRANTED &&
-                (granted[PermissionsAndroid.PERMISSIONS.NEARBY_WIFI_DEVICES] === PermissionsAndroid.RESULTS.GRANTED || Platform.Version < 33)
-            ) {
-                Alert.alert("Permissions granted");
-            } else {
-                Alert.alert("Permissions denied");
-            }
-        } catch (err) {
-            console.warn(err);
+        if (
+          granted[PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION] === PermissionsAndroid.RESULTS.GRANTED &&
+          (granted[PermissionsAndroid.PERMISSIONS.NEARBY_WIFI_DEVICES] === PermissionsAndroid.RESULTS.GRANTED || Platform.Version < 33)
+        ) {
+          Alert.alert("Permissions granted");
+        } else {
+          Alert.alert("Permissions denied");
         }
+      } catch (err) {
+        console.warn(err);
+      }
     }
-};
+  };
 
   const checkHotSpotActive = async () => {
     try {
       let isHotspotEnabled = await HotSpot.isHotspotEnabled()
+
       if (isHotspotEnabled) {
         return true
       }
-      Alert.alert("Pls enable hotspot","")
+      Alert.alert("Pls enable hotspot", "")
       return false
     } catch (error) {
       console.log("error", JSON.stringify(error))
@@ -90,24 +83,34 @@ function App(): React.JSX.Element {
   }
 
 
-  const getListOfConnectedDevice = async()=>{
+  const getListOfConnectedDevice = async () => {
     try {
+      setLoading(true)
       let hotspotStatus = await checkHotSpotActive()
-      if(hotspotStatus){
-        const connectedDevicesList = await NativeModules.HotspotModule.scanHotspotDevices();
-        console.log(connectedDevicesList)
-        Alert.alert("connectedDevicesList",JSON.stringify(connectedDevicesList))
+      if (hotspotStatus) {
+        const connectedDevices = await NativeModules.HotspotModule.scanHotspotDevices();
+        setConnectedDeviceList(connectedDevices)
       }
+      setLoading(false)
     } catch (error) {
-      console.log("error",JSON.stringify(error))
+      setLoading(false)
+      console.log("error", JSON.stringify(error))
     }
-  
+
   }
 
+  const getDeviceIp = async () => {
+    try {
+      let deviceIP = await NetworkInfo.getIPV4Address()
+      setDeviceIpAddress(deviceIP as string)
+    } catch (error) {
+      console.log("error", JSON.stringify(error))
+    }
+  }
 
   useEffect(() => {
-    getPermissions()
     requestPermissions()
+    getDeviceIp()
   }, [])
 
   return (
@@ -116,22 +119,61 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <TouchableOpacity
-      style={{}}
-      onPress={()=>getListOfConnectedDevice()}
-      >
-        <Text>Check Connected devices</Text>
-        </TouchableOpacity>   
+      <View style={styles.containerView}>
+        <Text>Device IPv4 : {deviceIpAddress}</Text>
+        {loading ?
+          <View>
+            <Text>loading... it will take some time pls wait</Text>
+            <ActivityIndicator size={"large"} color={"green"} />
+          </View>
+          :
+          <>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => getListOfConnectedDevice()}
+            >
+              <Text>Check Connected devices</Text>
+            </TouchableOpacity>
+            <FlatList
+              data={connectedDeviceList}
+              keyExtractor={(item) => item}
+              renderItem={(deviceIp) =>
+                <View style={ styles.innerView}>
+                  <Text>{deviceIp.item}</Text>
+                </View>
 
+              }
+            />
+          </>
+        }
+
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  innerView: { 
+      width: "100%", 
+      padding: 5, 
+      backgroundColor: "#f0f0f0", 
+      borderBottomWidth: 1 },
+  containerView: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
+  button: {
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: "#a2c4fc"
+  }
 
 });
 
